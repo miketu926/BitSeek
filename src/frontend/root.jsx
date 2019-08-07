ac87e1b7fc0b71eb36ac0aefe97080158080d923
Mike <miketu926@gmail.com>
2019-08-06 22:20:18 -0400

    removed react-router

--- src/frontend/root.jsx
+++ src/frontend/root.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import App from '../App'
-import { HashRouter } from 'react-router-dom'
 
 const Root = ({ store }) => {
 
   return (
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </Provider>
   )
 }
